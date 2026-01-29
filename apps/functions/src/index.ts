73f055df0e5554fc769229b5667671da8afa7140
rindo <rindo.katsura@gmail.com>
2026-01-29 12:15:46 +0900

    FunctionsエントリをHonoに切替

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import { getApps, initializeApp } from "firebase-admin/app";
-import cors from "cors";
 import { createApiHandler, createDefaultDeps } from "./api/handler.js";
 
 if (getApps().length === 0) {
@@ -10,13 +9,8 @@ if (getApps().length === 0) {
 }
 
 const handler = createApiHandler(createDefaultDeps());
-const corsHandler = cors({
-  origin: true,
-  methods: ["GET", "POST", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-});
 
 export const api = onRequest(async (req, res) => {
   logger.info("api called", { method: req.method, path: req.path });
-  return corsHandler(req, res, () => handler(req, res));
+  return handler(req, res);
 });
